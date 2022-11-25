48efd114419c726ec5d8c413601421bd222d6e06
noctarius <me@noctarius.com>
2022-11-25 13:47:02 +0100

    added a dedicated set of files for the timer codebase which is more commonly used now

---
+++ high-voltage-fan-controller/timer.c
@@ -0,0 +1,68 @@
+
+/*
+ * timer.c
+ */ 
+
+#include "atmel_start.h"
+#include "timer.h"
+
+
+uint64_t ticks;
+void vApplicationTickHook()
+{
+	ticks++;
+}
+
+int xTickToMs()
+{
+	return ticks / portTICK_RATE_MS;
+}
+
+/*
+ * @brief Timer Initialize
+ * @param  timer : pointer to a Timer structure
+ *         that contains the configuration information for the Timer.
+ */
+void TimerInit(Timer* timer) {
+	timer->end_time = 0;
+}
+
+/*
+ * @brief expired Timer
+ * @param  timer : pointer to a Timer structure
+ *         that contains the configuration information for the Timer.
+ */
+char TimerIsExpired(Timer* timer) {
+	long left = timer->end_time - ticks;
+	return (left < 0);
+}
+
+/*
+ * @brief Countdown millisecond Timer
+ * @param  timer : pointer to a Timer structure
+ *         that contains the configuration information for the Timer.
+ *         timeout : setting timeout millisecond.
+ */
+void TimerCountdownMS(Timer* timer, unsigned int timeout) {
+	timer->end_time = ticks + timeout;
+}
+
+/*
+ * @brief Countdown second Timer
+ * @param  timer : pointer to a Timer structure
+ *         that contains the configuration information for the Timer.
+ *         timeout : setting timeout millisecond.
+ */
+void TimerCountdown(Timer* timer, unsigned int timeout) {
+	timer->end_time = ticks + (timeout * 1000);
+}
+
+/*
+ * @brief left millisecond Timer
+ * @param  timer : pointer to a Timer structure
+ *         that contains the configuration information for the Timer.
+ */
+int TimerLeftMS(Timer* timer) {
+	long left = timer->end_time - ticks;
+	return (left < 0) ? 0 : left;
+}
---
+++ high-voltage-fan-controller/timer.h
@@ -0,0 +1,23 @@
+
+/*
+ * timer.h
+ */ 
+
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+int xTickToMs();
+
+typedef struct Timer Timer;
+struct Timer {
+	unsigned long systick_period;
+	unsigned long end_time;
+};
+
+void TimerInit(Timer*);
+char TimerIsExpired(Timer*);
+void TimerCountdownMS(Timer*, unsigned int);
+void TimerCountdown(Timer*, unsigned int);
+int TimerLeftMS(Timer*);
+
+#endif /* __TIMER_H__ */
